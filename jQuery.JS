614edb9d786fcb5c5f9575d178e63333c96a4a67
swapnilbarve <100339918+swapnilbarve@users.noreply.github.com>
2022-05-24 17:45:33 +0530

    Add files via upload

---
+++ jQuery.JS
@@ -0,0 +1,18 @@
+//1
+$("#hide").click(function(){
+    $("p").hide();
+   })
+   $("#show").click(function(){
+       $("p").show();
+      })
+   //2
+   $("#ajax").click(function(){
+       $.ajax({
+           url: "https://jsonplaceholder.typicode.com/todos/12",
+           type: "GET",
+           success: function(response){
+               console.log(response);
+               document.getElementById("learn").innerText = response.title;
+           }
+       })
+   })
